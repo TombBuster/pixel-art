8edc87b701fc0b0b8fb60387eb99dfdccafcac5a
Tom Bennett <thomas.bennett@williamhill.com>
2019-10-08 12:02:16 +0100

    with colour check bar

--- src/components/ColourChange.js
+++ src/components/ColourChange.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react'
 
 export class ColourChange extends Component {
 
+    boxStyle = () => {
+        
+        try{
+            return {
+                padding: '10px',
+                borderBottom: '1px #ccc dotted',
+                background: this.refs.input.value
+            }
+        }
+        catch(err) {
+            return {
+                padding: '10px',
+                borderBottom: '1px #ccc dotted',
+                background: 'black'
+            }
+        }
     
+    }
 
    
     setColourChange = () => {
@@ -11,7 +28,9 @@ export class ColourChange extends Component {
         // TODO: input validation, maybe fixed length hex?
     }
    
-    
+    changeDyn = () => {
+        this.refs.boxChange.style.background = this.refs.input.value;
+    }
    
     
    
@@ -21,8 +40,10 @@ export class ColourChange extends Component {
                 <input ref='input'
                 type="text"
                 placeholder="Colour..."
+                onChange={this.changeDyn}
                 />
                 <button onClick={this.setColourChange}>Apply</button>
+                <div ref='boxChange' style={this.boxStyle()}></div>
                 
             </div>
         )
